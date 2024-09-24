532be747d0c71bb0398ff0409c5c0a380fb3814e
gugunmo <131753649+gugunmo@users.noreply.github.com>
2024-09-24 15:27:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if source_radio == "IMAGE":
     
     if input is not None:
         uploaded_image = PIL.Image.open(input)
-        uploaded_image_cv =cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
+        uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
         visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
         st.image(visualized_image, channels = "BGR")
 
