0e9b4938b16356529216eb73bef4be3e52adc328
gugunmo <131753649+gugunmo@users.noreply.github.com>
2024-09-24 15:23:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import PIL
 from PIL import Image
-import opencv-python
 import cv2
+import opencv-python
 
 def play_video(video_source):
     camera = cv2.VideoCapture(video_source)
