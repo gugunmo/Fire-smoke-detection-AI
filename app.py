387ade2330fd8111b4e45cca57b8be10cc7e5c2d
gugunmo <131753649+gugunmo@users.noreply.github.com>
2024-09-24 15:18:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-import streamlit as st
-import cv2
-import PIL
-from PIL import Image
-import numpy
-import utils
-import io
-import gradio
-
 def play_video(video_source):
     camera = cv2.VideoCapture(video_source)
 
